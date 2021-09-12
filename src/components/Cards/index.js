77bd82c3e65e53977533872417d1d7c03c64d041
vinicius <viniciusbraznick@gmail.com>
2021-09-11 23:46:56 -0300

    dynamic card inclusion

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,102 +1,32 @@
 import React from 'react';
+import {card_data} from '../../data.js';
 
 import optionIcon from '../../assets/icon-ellipsis.svg';
 import './style.scss';
 
+
 export default function Cards() {
 	return(
 		<div className="cards-container">
-			
-			<section>
-				<div className="card-content">
-					<div className="card-header">
-						<span>Work</span>
-						<img src={optionIcon} alt="options" width="20px" height="5px"/>
-					</div>
-					<div className="card-hours">
-						<h3>32Hrs</h3>
-						<div className="card-hours__range">
-							<p>Last Week - 5hrs</p>
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<section>
-				<div className="card-content">
-					<div className="card-header">
-						<span>Work</span>
-						<img src={optionIcon} alt="options" width="20px" height="5px"/>
-					</div>
-					<div className="card-hours">
-						<h3>32Hrs</h3>
-						<div className="card-hours__range">
-							<p>Last Week - 5hrs</p>
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<section>
-				<div className="card-content">
-					<div className="card-header">
-						<span>Work</span>
-						<img src={optionIcon} alt="options" width="20px" height="5px"/>
-					</div>
-					<div className="card-hours">
-						<h3>32Hrs</h3>
-						<div className="card-hours__range">
-							<p>Last Week - 5hrs</p>
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<section>
-				<div className="card-content">
-					<div className="card-header">
-						<span>Work</span>
-						<img src={optionIcon} alt="options" width="20px" height="5px"/>
-					</div>
-					<div className="card-hours">
-						<h3>32Hrs</h3>
-						<div className="card-hours__range">
-							<p>Last Week - 5hrs</p>
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<section>
-				<div className="card-content">
-					<div className="card-header">
-						<span>Work</span>
-						<img src={optionIcon} alt="options" width="20px" height="5px"/>
-					</div>
-					<div className="card-hours">
-						<h3>32Hrs</h3>
-						<div className="card-hours__range">
-							<p>Last Week - 5hrs</p>
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<section>
-				<div className="card-content">
-					<div className="card-header">
-						<span>Work</span>
-						<img src={optionIcon} alt="options" width="20px" height="5px"/>
-					</div>
-					<div className="card-hours">
-						<h3>32Hrs</h3>
-						<div className="card-hours__range">
-							<p>Last Week - 5hrs</p>
-						</div>
-					</div>
-				</div>
-			</section>
-
+			{card_data.map(card => {
+					return(
+						<section>
+							<div className="card-content">
+								<div className="card-header">
+									<span>{card.title}</span>
+									<img src={optionIcon} alt="options" width="20px" height="5px"/>
+								</div>
+								<div className="card-hours">
+									<h3>{card.timeframes.daily.current}Hrs</h3>
+									<div className="card-hours__range">
+										<p>Last Week - {card.timeframes.daily.previous}hrs</p>
+									</div>
+								</div>
+							</div>
+						</section>
+					)
+				})
+			}
 		</div>
 	)
 }
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-[
+export const card_data =[
   {
     "title": "Work",
     "timeframes": {
